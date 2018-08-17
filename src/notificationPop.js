5159028a6b892562eb85d0209d212f227da0ca00
Matt Simcox <mattsimcox13@gmail.com>
2018-08-17 18:52:00 -0400

    Updoot to ideas on ipc stuff

--- src/notificationPop.js
+++ src/notificationPop.js
@@ -1,1 +1,7 @@
 // TODO: IDFK, keep trying ipcRenderer, ipcMain and remote I guess
+// NOTE: Try the following:
+/*
+  https://www.w3schools.com/js/js_htmldom_events.asp
+  https://medium.freecodecamp.org/how-to-build-your-first-app-with-electron-41ebdb796930
+  https://www.christianengvall.se/ipcmain-and-ipcrenderer/
+*/
