eca453e6aac25c0cbb93711b4bfe2acf2d8b4cce
Matt Simcox <mattsimcox13@gmail.com>
2018-08-18 12:02:54 -0400

    Functioning onclick handlers on the buttons

--- src/notificationPop.js
+++ src/notificationPop.js
@@ -1,21 +1,13 @@
-// TODO: IDFK, keep trying ipcRenderer, ipcMain and remote I guess
-// NOTE: Try the following:
-/*
-  https://www.w3schools.com/js/js_htmldom_events.asp
-  https://medium.freecodecamp.org/how-to-build-your-first-app-with-electron-41ebdb796930
-  https://www.christianengvall.se/ipcmain-and-ipcrenderer/
-*/
+// NOTE: Had to put onclick handler in the physical html connected to btn tag
 const {ipcRenderer} = require('electron')
 
-// DIS SHIT WORKS
-window.onclick = myFunction;
-
-// If the user clicks in the window, set the background color of <body> to yellow
-function myFunction() {
-    document.getElementById("okBtn").style.backgroundColor = "yellow";
+// Send code to main to close notWin browserWindow instance
+function okBtn() {
+  
 }
 
-// Can try the following either in this file or in script tags
-// document.getElementById("about").addEventListener("click", function (e) {
-//    ipcRenderer.send('show-about-window-event');
-// }
+// Send code to main to close notWin broswerWindow instance and
+// Reopen win, the main broswerWindow instance
+function cancelBtn() {
+
+}
